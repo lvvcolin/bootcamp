04daa72231300269206fa39f0960f618925232fa
Carlijn den Hartog <s1158298@student.windesheim.nl>
2021-03-06 21:23:57 +0100

    email 'ends_with' & minor color change in home.blade

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            // 'email' => 'required|email|ends_with:student.windesheim.com|max:255',
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'ends_with:@student.windesheim.nl'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
