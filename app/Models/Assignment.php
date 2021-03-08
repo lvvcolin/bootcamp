e8c19e055645ab26246ced82f660cd1f084156b5
Diamé Morreau <diamemorreau@me.com>
2021-03-08 15:53:18 +0100

    migrations aangemaakt en ingevuld aan de hand van de ERD. Models opgezet en relaties gelegd. Controllers aangemaakt.

---
+++ app/Models/Assignment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    // an assignment belongs to a course 
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    // an assignment has many files
+    public function files()
+    {
+        return $this->hasMany(File::class);
+    }
+
+    // This is the pivot table connection with User model
+    public function users()
+    {
+    	return $this->belongsToMany(User::class);
+    }
+
+}
