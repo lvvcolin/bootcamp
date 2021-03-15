fe4fbb0ff42026b37b3e38c80115a90a1a862192
THExNEXTxLEGEND <s1158994@student.windesheim.nl>
2021-03-15 19:05:23 +0100

    Basisopzet course (ignore de veranderingen in welkom.blade , ik heb het daarin getest omdat de login functie bij mij niet goed werkt

---
+++ resources/views/Course.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/-JnUHKu-Ps8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+@endsection
