99c6a1355f63799103cf00c50baca5df6e3b40a6
Mkirez <m.kirez@hotmail.com>
2021-04-22 03:19:07 +0200

    Small fixes
    Welcome page and dropdown menu fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,11 +61,18 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
-                                        <a class="dropdown-item" href="{{ route('profile', auth()->user()) }}">
+                                    <a class="dropdown-item" href="{{ route('profile', auth()->user()) }}">
                                         
                                         {{ __('Profile') }}
                                         
-                                        </a>
+                                    </a>
+
+                                    <a class="dropdown-item" href="{{ route('course_index', auth()->user()) }}">
+                                        
+                                        {{ __('Course') }}
+                                        
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
